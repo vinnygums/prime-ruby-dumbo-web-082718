9f4a69bfe2f54409f904857e1f75ddfa7fb096d5
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-20 17:11:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+require "pry"
+def prime?(integer)
+  if integer <= 1
+    return false
+  else
+    (2..integer-1).to_a.all? do |divisor|
+    #if integer % divisor == 0
+      #return false
+    #else
+      #binding.pry
+      #return true
+    #end
+    integer % divisor != 0
+  end
+  end
+end
