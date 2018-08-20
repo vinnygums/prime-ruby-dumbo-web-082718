4cbd00970e14d6f53cba6a69639cb57d6d99ffcc
Vicente Soriano <vicente.soriano@icloud.com>
2018-08-20 16:07:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,8 @@ def prime?(integer)
   (2..integer).each do |divisor|
     if integer == 2
       return true
-    elsif integer % divisor == 0
+      
+    elsif integer % divisor == 0 || integer
       return false
     else 
       return true
